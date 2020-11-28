63efd0bcb86dd68c4b645099a5589b6c06b3f7fe
zoriankaH <60688935+zoriankaH@users.noreply.github.com>
2020-11-28 12:44:15 +0200

    Update BaseArray.java

--- src/main/java/ua/edu/ucu/smartarr/BaseArray.java
+++ src/main/java/ua/edu/ucu/smartarr/BaseArray.java
@@ -1,7 +1,7 @@
 package ua.edu.ucu.smartarr;
 
 // Base array for decorators
-public class BaseArray implements SmartArray{
+public class BaseArray implements SmartArray {
     private Object[] baseArray;
 
     public BaseArray(Object[] array) {
