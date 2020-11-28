ddc53340268f43626bf02edb0e5659c1fbb6f166
zoriankaH <60688935+zoriankaH@users.noreply.github.com>
2020-11-28 12:32:05 +0200

    Update SortDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/SortDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/SortDecorator.java
@@ -1,6 +1,33 @@
 package ua.edu.ucu.smartarr;
 
+import ua.edu.ucu.functions.MyComparator;
+
+import java.util.Arrays;
+
 // Sorts elements using MyComparator to compare them
-public class SortDecorator {
-    
+public class SortDecorator extends SmartArrayDecorator{
+    private Object[] begrray;
+    private final MyComparator comparator;
+
+    public SortDecorator(SmartArray smartArray, MyComparator compar) {
+        super(smartArray);
+        begrray = smartArray.toArray();
+        comparator = compar;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Arrays.sort(begrray, comparator);
+        return begrray;
+    }
+
+    @Override
+    public String operationDescription() {
+        return "All elements in array are sorted by comparator";
+    }
+
+    @Override
+    public int size() {
+        return begrray.length;
+    }
 }
