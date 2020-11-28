6d19da4b00f2b1aac82c3100a630c8af3621ef99
zoriankaH <60688935+zoriankaH@users.noreply.github.com>
2020-11-28 12:31:11 +0200

    Update FilterDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/FilterDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/FilterDecorator.java
@@ -1,6 +1,47 @@
 package ua.edu.ucu.smartarr;
 
+import ua.edu.ucu.functions.MyPredicate;
+
 // Tests every element and removes it if it doesn't satisfy MyPredicate
-public class FilterDecorator {
-   
+public class FilterDecorator extends SmartArrayDecorator {
+    private Object[] begrray;
+    private MyPredicate predicate;
+    private int newLenght;
+
+    public FilterDecorator(SmartArray smartArr, MyPredicate myPredicate) {
+        super(smartArr);
+        begrray = smartArray.toArray();
+        predicate = myPredicate;
+    }
+
+    @Override
+    public Object[] toArray() {
+        Object[] newAr = new Object[size()];
+        int newSize = 0;
+        for (int i = 0; i < begrray.length; i++) {
+            if(predicate.test(begrray[i])) {
+                newAr[newSize] = begrray[i];
+                newSize++;
+            }
+        }
+        newLenght = newSize;
+        return newAr;
+    }
+
+    @Override
+    public String operationDescription() {
+        return "All elements in array satisfy MyPredicate.";
+    }
+
+    @Override
+    public int size() {
+        int newSize = 0;
+        for (int i = 0; i < begrray.length; i++) {
+            if(predicate.test(begrray[i])) {
+                newSize++;
+            }
+        }
+        newLenght = newSize;
+        return newLenght;
+    }
 }
