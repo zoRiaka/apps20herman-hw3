a5b0865192886a10f574535147e036ef9ee9daa5
zoriankaH <60688935+zoriankaH@users.noreply.github.com>
2020-11-28 12:32:25 +0200

    Update SmartArrayApp.java

--- src/main/java/ua/edu/ucu/SmartArrayApp.java
+++ src/main/java/ua/edu/ucu/SmartArrayApp.java
@@ -4,12 +4,13 @@ import java.util.Arrays;
 import ua.edu.ucu.functions.MyComparator;
 import ua.edu.ucu.functions.MyFunction;
 import ua.edu.ucu.functions.MyPredicate;
+import ua.edu.ucu.smartarr.*;
 
 public class SmartArrayApp {
 
     public static Integer[]
             filterPositiveIntegersSortAndMultiplyBy2(Integer[] integers) {
-                
+
         MyPredicate pr = new MyPredicate() {
             @Override
             public boolean test(Object t) {
@@ -35,10 +36,13 @@ public class SmartArrayApp {
         SmartArray sa = new BaseArray(integers);
 
         sa = new FilterDecorator(sa, pr); // Result: [2, 1, 3];
+        System.out.println(sa.toArray());
         sa = new SortDecorator(sa, cmp); // Result: [1, 2, 3]
+        System.out.println(sa.toArray());
         sa = new MapDecorator(sa, func); // Result: [2, 4, 6]
+        System.out.println(sa.toArray());
 
-        // Alternative
+//         Alternative
 //        sa = new MapDecorator(
 //                    new SortDecorator(
 //                        new FilterDecorator(sa, pr),
@@ -50,10 +54,47 @@ public class SmartArrayApp {
 
     public static String[]
             findDistinctStudentNamesFrom2ndYearWithGPAgt4AndOrderedBySurname(Student[] students) {
+        int year = 2;
+        int average = 4;
+        SmartArray studentsArray = new BaseArray(students);
+        studentsArray = new DistinctDecorator(studentsArray);
+
+        MyPredicate myPredicate = new MyPredicate() {
+            @Override
+            public boolean test(Object student) {
+                if(((Student) student).getYear() == year && ((Student) student).getGPA() >= average) {
+                        return true;
+                    }
+                return false;
+            }
+        };
+
+        studentsArray = new FilterDecorator(studentsArray, myPredicate);
+
+
+        MyComparator myComparator = new MyComparator() {
+            @Override
+            public int compare(Object student1, Object student2) {
+                String st2 = ((Student) student2).getSurname();
+                return ((Student) student1).getSurname().compareTo(st2);
+            }
+        };
+
+        studentsArray = new SortDecorator(studentsArray, myComparator);
+
+        MyFunction myFunction = new MyFunction() {
+            @Override
+            public Object apply(Object student) {
+                return ((Student)student).getSurname() + " " + ((Student)student).getName();
+            }
+        };
+
+        studentsArray = new MapDecorator(studentsArray, myFunction);
+        studentsArray = new DistinctDecorator(studentsArray);
 
         // Hint: to convert Object[] to String[] - use the following code
-        //Object[] result = studentSmartArray.toArray();
-        //return Arrays.copyOf(result, result.length, String[].class);
-        return null;
+        Object[] result = studentsArray.toArray();
+        return Arrays.copyOf(result, result.length, String[].class);
+
     }
 }
