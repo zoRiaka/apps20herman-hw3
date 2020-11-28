9829af53d6526956b62e655eca3da31526ab0221
zoriankaH <60688935+zoriankaH@users.noreply.github.com>
2020-11-28 12:30:51 +0200

    Update DistinctDecorator.java

--- src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
+++ src/main/java/ua/edu/ucu/smartarr/DistinctDecorator.java
@@ -1,6 +1,31 @@
 package ua.edu.ucu.smartarr;
 
-// Remove duplicates from SmartArray. Use method equals() to compare objects
-public class DistinctDecorator {
+import java.util.Arrays;
+import java.util.LinkedHashSet;
 
+// Remove duplicates from SmartArray. Use method equals() to compare objects
+public class DistinctDecorator extends SmartArrayDecorator{
+    private Object[] begrray;
+
+    public DistinctDecorator(SmartArray smartArray) {
+        super(smartArray);
+        begrray = smartArray.toArray();
+    }
+
+    @Override
+    public Object[] toArray() {
+        LinkedHashSet<Object> linkedHashSet = new LinkedHashSet<>(Arrays.asList(begrray));
+        Object[] withoutDuplicates = linkedHashSet.toArray(new Object[] {});
+        return withoutDuplicates;
+    }
+
+    @Override
+    public String operationDescription() {
+        return "All elements in array are unique.";
+    }
+
+    @Override
+    public int size() {
+        return new DistinctDecorator(smartArray).toArray().length;
+    }
 }
