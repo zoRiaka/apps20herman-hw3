cbed174e258169605fba5452f0ba5128fa08fd25
zoriankaH <60688935+zoriankaH@users.noreply.github.com>
2020-11-28 12:30:34 +0200

    Update BaseArray.java

--- src/main/java/ua/edu/ucu/smartarr/BaseArray.java
+++ src/main/java/ua/edu/ucu/smartarr/BaseArray.java
@@ -1,6 +1,25 @@
 package ua.edu.ucu.smartarr;
 
 // Base array for decorators
-public class BaseArray {
+public class BaseArray implements SmartArray{
+    private Object[] baseArray;
 
+    public BaseArray(Object[] array) {
+        baseArray = array;
+    }
+
+    @Override
+    public Object[] toArray() {
+        return baseArray;
+    }
+
+    @Override
+    public String operationDescription() {
+        return "The array is not changed";
+    }
+
+    @Override
+    public int size() {
+        return baseArray.length;
+    }
 }
